87f492a38a6242d34b0e35ee9e708b00f2728243
Nils Philippsen <nils@redhat.com>
2020-03-19 12:41:02 +0100

    Add setup.py
    This allows us using the normal Python build macros when building the
    RPM package.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os.path
+
+from setuptools import setup
+
+
+HERE = os.path.dirname(__file__)
+with open(os.path.join(HERE, "requirements.txt"), "r") as f:
+    INSTALL_REQUIRES = [x.strip() for x in f.readlines()]
+with open(os.path.join(HERE, "test_requirements.txt"), "r") as f:
+    TESTS_REQUIRE = [x.strip() for x in f.readlines()]
+
+
+setup(
+    name="rpmautospec",
+    version="0.0.1",
+    description="Package and CLI tool for generating RPM releases and changelogs",
+    # Possible options are at https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    license="CC0",
+    maintainer="Fedora Infrastructure Team",
+    maintainer_email="infrastructure@lists.fedoraproject.org",
+    platforms=["Fedora", "GNU/Linux"],
+    url="https://pagure.io/Fedora-Infra/rpmautospec",
+    keywords="fedora",
+    packages=["rpmautospec"],
+    include_package_data=False,
+    package_data={},
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    entry_points={"console_scripts": ["rpmautospec = rpmautospec.cli:main",],},
+)

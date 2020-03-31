1405ef60b55e4c8fc031c782ca2a84abcca73198
Nils Philippsen <nils@redhat.com>
2020-03-31 14:09:20 +0200

    Consistently use `_log` for logging
    Also, create `_log` globally in the module and not for every invocation
    of the callback.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- koji_plugins/rpmautospec_hub.py
+++ koji_plugins/rpmautospec_hub.py
@@ -11,19 +11,19 @@ from rpmautospec.py2compat import escape_tags
 CONFIG_FILE = "/etc/koji-hub/plugins/rpmautospec_hub.conf"
 CONFIG = None
 
+_log = logging.getLogger("koji.plugin.rpmautospec_hub")
+
 
 @callback("postTag")
 def autotag_cb(cb_type, **kwargs):
     global CONFIG
-
-    log = logging.getLogger("koji.plugin.rpmautospec_hub")
 
     if not CONFIG:
         try:
             CONFIG = koji.read_config_files([(CONFIG_FILE, True)])
         except Exception:
             message = "While attempting to read config file %s, an exception occurred:"
-            log.exception(message, CONFIG_FILE)
+            _log.exception(message, CONFIG_FILE)
             return
 
     base_url = CONFIG.get("pagure", "url")
@@ -45,10 +45,10 @@ def autotag_cb(cb_type, **kwargs):
         commit = match.group("commit")
         if not repo or not commit:
             info = "Could not parse repo and commit from {}, skipping."
-            log.info(info.format(build["source"]))
+            _log.info(info.format(build["source"]))
             return
     else:
-        log.info("No source for this build, skipping.")
+        _log.info("No source for this build, skipping.")
         return
 
     build["epoch"] = build.get("epoch") or 0
@@ -73,10 +73,10 @@ def autotag_cb(cb_type, **kwargs):
         response = requests.post(endpoint_url, headers=headers, data=data)
     except Exception:
         error = "While attempting to create a tag in %s, an exception occurred:"
-        log.exception(error, endpoint_url)
+        _log.exception(error, endpoint_url)
         return
 
     if not response.ok:
         error = "While attempting to create a tag in %s, the request failed with: STATUS %s %s"
-        log.error(error, endpoint_url, response.status_code, response.text)
+        _log.error(error, endpoint_url, response.status_code, response.text)
         return

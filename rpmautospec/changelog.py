67c24c6e79d303eff8cb7f8d3d636aa2385de49f
Pierre-Yves Chibon <pingou@pingoured.fr>
2020-04-08 10:36:19 +0200

    Only sort builds when there are several of them
    This is apparently a very small optimisation but one nonetheless.
    
    Signed-off-by: Pierre-Yves Chibon <pingou@pingoured.fr>

--- rpmautospec/changelog.py
+++ rpmautospec/changelog.py
@@ -155,7 +155,8 @@ def produce_changelog(repopath, latest_rel=None):
                 output.append(entry)
                 entry = []
                 # Use the most recent build for EVR
-                builds = [parse_evr(nevrd_to_evr(b)) for b in tags[commithash]]
+                if len(builds) > 1:
+                    builds = [parse_evr(nevrd_to_evr(b)) for b in tags[commithash]]
                 _log.debug("Builds to sort: %s", builds)
                 builds.sort(key=rpmvercmp_key, reverse=True)
                 _epo, _ver, _rel = builds[0]

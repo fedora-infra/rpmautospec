d3ffcfa67248e4416e0fe03d4a956961f7dccc37
Nils Philippsen <nils@redhat.com>
2021-06-16 12:33:23 +0200

    Add choice between number only or complete release
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- rpmautospec/subcommands/release.py
+++ rpmautospec/subcommands/release.py
@@ -23,16 +23,37 @@ def register_subcommand(subparsers):
         help="Path to package worktree or the spec file within",
     )
 
+    complete_release_group = calc_release_parser.add_mutually_exclusive_group()
+
+    complete_release_group.add_argument(
+        "-c",
+        "--complete-release",
+        action="store_true",
+        default=True,
+        help="Print the complete release with flags (without dist tag)",
+    )
+
+    complete_release_group.add_argument(
+        "-n",
+        "--number-only",
+        action="store_false",
+        dest="complete_release",
+        default=False,
+        help="Print only the calculated release number",
+    )
+
     return subcmd_name
 
 
-def calculate_release(spec_or_path: Union[str, Path]) -> int:
+def calculate_release(
+    spec_or_path: Union[str, Path], *, complete_release: bool = True
+) -> Union[str, int]:
     processor = PkgHistoryProcessor(spec_or_path)
     result = processor.run(visitors=(processor.release_number_visitor,))
-    return result["release-complete"]
+    return result["release-complete" if complete_release else "release-number"]
 
 
 def main(args):
     """Main method."""
-    release = calculate_release(args.spec_or_path)
+    release = calculate_release(args.spec_or_path, complete_release=args.complete_release)
     log.info("calculate_release release: %s", release)

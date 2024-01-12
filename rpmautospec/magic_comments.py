beb16792ca630fbbaea058712a4854957aada9df
Nils Philippsen <nils@redhat.com>
2024-01-12 22:46:32 +0100

    Add [bump release: N] magic comment
    This bumps the release to a value of at least N.
    
    Fixes: #53
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- rpmautospec/magic_comments.py
+++ rpmautospec/magic_comments.py
@@ -3,19 +3,24 @@ from typing import NamedTuple
 
 magic_comment_re = re.compile(r"^\s*\[(?P<magic>.*)\]\s*$")
 skip_changelog_re = re.compile(r"\s*skip\s+changelog\s*")
+bump_release_re = re.compile(r"\s*bump\s+release\s*(?::\s*)?(?P<bump_value>\d+)\s*")
 
 
 class MagicCommentResult(NamedTuple):
     skip_changelog: bool
+    bump_release: int
 
 
 def parse_magic_comments(message: str) -> MagicCommentResult:
     skip_changelog = False
+    bump_release = 0
 
     for line in message.split("\n"):
         if l_match := magic_comment_re.match(line):
             for part in l_match.group("magic").split(","):
                 if skip_changelog_re.match(part):
                     skip_changelog = True
+                if br_match := bump_release_re.match(part):
+                    bump_release = max(bump_release, int(br_match.group("bump_value")))
 
-    return MagicCommentResult(skip_changelog=skip_changelog)
+    return MagicCommentResult(skip_changelog=skip_changelog, bump_release=bump_release)

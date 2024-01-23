d5739b5cd9ac4271c5c74fa1736778ee757595ae
Nils Philippsen <nils@redhat.com>
2024-01-23 11:17:32 +0100

    Cover all rpmautospec.cli code in tests
    Also, avoid impossible to reach branch in code.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- tests/rpmautospec/test_cli.py
+++ tests/rpmautospec/test_cli.py
@@ -1,9 +1,69 @@
+import logging
 import subprocess
 import sys
+from unittest import mock
 
 import pytest
 
+from rpmautospec import cli
+
 from . import temporary_cd
+
+
+@pytest.mark.parametrize("log_level_name", ("CRITICAL", "WARNING", "INFO", "DEBUG"))
+def test_setup_logging(log_level_name):
+    """Test the setup_logging() function."""
+    log_level = getattr(logging, log_level_name)
+
+    with mock.patch.object(cli.logging, "StreamHandler") as StreamHandler, mock.patch.object(
+        cli.logging, "lastResort"
+    ) as lastResort, mock.patch.object(cli.logging, "basicConfig") as basicConfig:
+        info_handler = StreamHandler.return_value
+        cli.setup_logging(log_level)
+
+    StreamHandler.assert_called_once_with(stream=sys.stdout)
+
+    info_handler.setLevel.assert_called_with(log_level)
+    info_handler.addFilter.assert_called_once()
+
+    lastResort.addFilter.assert_called_once()
+
+    if log_level_name == "INFO":
+        info_handler.setFormatter.assert_called_once()
+        lastResort.setFormatter.assert_called_once()
+    else:
+        info_handler.setFormatter.assert_not_called()
+        lastResort.setFormatter.assert_not_called()
+
+    basicConfig.assert_called_once_with(level=log_level, handlers=[info_handler, lastResort])
+
+
+@pytest.mark.parametrize("exception_cls", (None, BrokenPipeError, OSError))
+def test_handle_expected_exceptions(exception_cls):
+    """Test the handle_expected_exceptions context manager."""
+    with mock.patch.object(cli.sys, "exit") as sys_exit:
+        with cli.handle_expected_exceptions():
+            if exception_cls:
+                raise exception_cls("BOO")
+
+    if exception_cls != OSError:
+        sys_exit.assert_not_called()
+    else:
+        sys_exit.assert_called_once_with("error: BOO")
+
+
+def test_main_nothing():
+    """Test running the CLI without arguments."""
+    completed = subprocess.run(
+        [sys.executable, "-c", "from rpmautospec import cli; cli.main()"],
+        check=False,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+
+    assert completed.returncode != 0
+    assert "error: the following arguments are required: subcommand" in completed.stderr
 
 
 def test_main_help():

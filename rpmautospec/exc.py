8f09ca6ac81c0e964af66739b6cbff5b71af1cb3
Nils Philippsen <nils@redhat.com>
2024-02-15 23:50:38 +0100

    Add details to custom exceptions
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- rpmautospec/exc.py
+++ rpmautospec/exc.py
@@ -1,5 +1,18 @@
+from typing import Optional
+
+
 class RpmautospecException(Exception):
     """Base class for rpmautospec exceptions."""
+
+    def __init__(self, *args, code: Optional[str] = None, detail: Optional[str] = None):
+        super().__init__(*args)
+        self.code = code
+        self.detail = detail
+
+    def __str__(self):
+        if self.detail:
+            return f"{', '.join(self.args)}:\n{self.detail}"
+        return super().__str__()
 
 
 class SpecParseFailure(RpmautospecException):

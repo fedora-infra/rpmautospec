fb235eeac465882cf694c49d473e208724e46c80
Nils Philippsen <nils@redhat.com>
2024-08-08 15:14:24 +0200

    Ignore unused imports in all __init__.py files
    This is so these imports don’t have to be marked explicitly
    “intentionally unused”.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- rpmautospec/__init__.py
+++ rpmautospec/__init__.py
@@ -1,6 +1,6 @@
 # Import this for compatibility
-from rpmautospec_core import specfile_uses_rpmautospec  # noqa: F401
+from rpmautospec_core import specfile_uses_rpmautospec
 
-from .subcommands.process_distgit import process_distgit  # noqa: F401
-from .subcommands.release import calculate_release, calculate_release_number  # noqa: F401
-from .version import __version__  # noqa: F401
+from .subcommands.process_distgit import process_distgit
+from .subcommands.release import calculate_release, calculate_release_number
+from .version import __version__

277a9e75e00d375104cf8942a30e6fc6cb28d95b
Nils Philippsen <nils@redhat.com>
2025-04-08 09:16:35 +0200

    Make robust against missing package metadata
    Previously, the version module relies on importlib.metadata() to work.
    Falling back to packages not “properly installed”, i.e. without
    metadata, makes the usual method fail. Provide a fallback value – which
    can be filled in with the real one (e.g. using sed) while packaging.
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- rpmautospec/version.py
+++ rpmautospec/version.py
@@ -1,4 +1,7 @@
 from importlib import metadata
 
-__version__ = metadata.version("rpmautospec")
+try:
+    __version__ = metadata.version("rpmautospec")
+except Exception:  # pragma: no cover
+    __version__ = "0.0.0"
 __version_info__ = tuple(int(x) for x in __version__.split("."))

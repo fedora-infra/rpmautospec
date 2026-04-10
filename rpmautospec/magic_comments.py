8856e4bbba6fe345894b10cc305a5149d2f8969a
Petr Menšík <pemensik@redhat.com>
2026-04-10 15:31:57 +0200

    Initial implementation of rightmost bumps
    Adds new magic comments supported in commits:
    
    [bump_rightmost: 3]
    [start_rightmost]
    
    This should implement missing z-stream type bumps used mainly in RHEL
    already released branches fixes.
    
    It is needed to ensure latest branch build is always preferred after
    upgrade to higher version. In Fedora regular mass rebuilds mitigates the
    need for something similar. But it could be still used when doing rebase
    in multiple branches, where only rawhide build should always have
    highest release.
    
    This is attempt to provide user-friendly alternative to manual calls of
    rpmdev-bumpspec -r command.
    
    Signed-off-by: Petr Menšík <pemensik@redhat.com>

--- rpmautospec/magic_comments.py
+++ rpmautospec/magic_comments.py
@@ -1,26 +1,59 @@
 import re
-from typing import NamedTuple
+import typing
 
 magic_comment_re = re.compile(r"^\s*\[(?P<magic>.*)\]\s*$")
-skip_changelog_re = re.compile(r"\s*skip\s+changelog\s*")
-bump_release_re = re.compile(r"\s*bump\s+release\s*(?::\s*)?(?P<bump_value>\d+)\s*")
+intvalue_re = re.compile(r"\s*(?P<keyname>[a-z]+([\s_]+[a-z]+)*)\s*(?::\s*)?(?P<value>\d+)\s*")
 
 
-class MagicCommentResult(NamedTuple):
-    skip_changelog: bool
-    bump_release: int
+class MagicCommentResult(typing.NamedTuple):
+    """Stores special commit parameters extracted from a commit message."""
+
+    skip_changelog: bool = False
+    """Do not include this commit in changelog."""
+    start_rightmost: bool = False
+    """Start increasing number on the right of release string, not left."""
+    bump_release: int = 0
+    """Bump release to specified number."""
+    bump_rightmost: int = 0
+    """Bump rightmost (minorbump) of the release to specified number."""
 
 
 def parse_magic_comments(message: str) -> MagicCommentResult:
-    skip_changelog = False
-    bump_release = 0
+    """Parses commit message for special lines providing extra features.
+
+    Reworked to extract keywords from MagicCommentResult directly.
+    Should reduce code repetition and simplify documentation of supported keywords.
+
+    It does so by replacing spaces with _ underscore, then searching in defined
+    class properties. Do not define extra regex for everything.
+
+    Supports parsing of bool presences of:
+
+       [skip changelog]
+       [start rightmost]
+
+    Supports parsing of int values of:
+
+       [bump release: 3]
+       [bump rightmost: 4]
+"""
+
+    types = typing.get_type_hints(MagicCommentResult)
+    values = dict()
 
     for line in message.split("\n"):
         if l_match := magic_comment_re.match(line):
             for part in l_match.group("magic").split(","):
-                if skip_changelog_re.match(part):
-                    skip_changelog = True
-                if br_match := bump_release_re.match(part):
-                    bump_release = max(bump_release, int(br_match.group("bump_value")))
+                part = part.strip().replace(" ", "_")
+                if part in types and types[part] is bool:
+                    values[part] = True
+                elif br_match := intvalue_re.match(part):
+                    keyname = br_match.group("keyname")
+                    if keyname in types and types[keyname] is int:
+                        values[keyname] = int(br_match.group("value"))
+                    else:
+                        raise ValueError(
+                            f"Parameter \"{keyname}\" is not recognized int type magic"
+                        )
 
-    return MagicCommentResult(skip_changelog=skip_changelog, bump_release=bump_release)
+    return MagicCommentResult(**values)
